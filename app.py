0599876c2913f0c556220efd071d1dbce4cb2fc3
Your Name <you@example.com>
2023-12-09 10:46:22 -0800

    Complete forms

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, session, flash
 from models import connect_db, db, User
-from forms import RegisterForm
-# from forms import RegisterForm, LoginForm
+from forms import RegisterForm, LoginForm, FeedbackForm, DeleteForm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth-auth"
@@ -34,7 +33,7 @@ render_template('register.html', form=form)
 def submit_registration_form():
     """Handle new user form submission, redirect to /secret"""
 
-form = UserForm()
+form = RegisterForm()
 if form.validate_on_submit():
 
     username = form.username.data
@@ -51,13 +50,29 @@ if form.validate_on_submit():
 @app.route('/login')
 def show_login_form():
     """Display form for logging in existing user"""
-    render_template('login.html')
+    form = LoginForm()
+    
+    render_template('login.html', form=form)
 
 @app.route('/login', methods=["POST"])
 def submit_login_form():
     """Handle login form submission, redirect to /secret"""
+    form = LoginForm()
+    if form.validate_on_submit():
 
-@app.route('/secret')
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            flash(f"Welcome Back, {user.username}!", "primary")
+            session['user_id'] = user.id
+            return redirect('/tweets')
+        else:
+            form.username.errors = ['Invalid username/password.']
+
+    return render_template('login.html', form=form)
+
 def display_secret_content():
     """secret landing page for once a user is registered/logged in"""
 
--- forms.py
+++ forms.py
@@ -13,3 +13,12 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
+
+class FeedbackForm(FlaskForm):
+    id = IntegerField("user id", validators=[InputRequired()])
+    title = StringField("Title", validators=[InputRequired()])
+    content = StringField("Content", validators=[InputRequired()])
+    username = StringField("username", validators=[InputRequired()])
+
+class DeleteForm(FlaskForm):
+    """Delete form -- this form is intentionally blank."""
