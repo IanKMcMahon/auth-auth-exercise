6858c2408474c8ed19527eb3a9f3b97f779d7fec
Your Name <you@example.com>
2023-12-09 07:53:56 -0800

    app boilerplate

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, session, flash
+from models import connect_db, db, User
+# from forms import RegisterForm, LoginForm
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth-auth"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "ihaveasecret"
+app.app_context.push()
+
+connect_db(app)
+db.create_all()
+
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+# **GET */ :*** Redirect to /register.
+
+# **GET */register :*** Show a form that when submitted will register/create a user. This form should accept a username, password, email, first_name, and last_name. Make sure you are using WTForms and that your password input hides the characters that the user is typing!
+
+# **POST */register :*** Process the registration form by adding a new user. Then redirect to ***/secret***
+
+# **GET */login :*** Show a form that when submitted will login a user. This form should accept a username and a password. Make sure you are using WTForms and that your password input hides the characters that the user is typing!
+
+# **POST */login :*** Process the login form, ensuring the user is authenticated and going to ***/secret*** if so.
+
+# **GET */secret :*** Return the text “You made it!” (don’t worry, we’ll get rid of this soon)
