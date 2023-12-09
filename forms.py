e4529b2a6db7d2da0ff046d79e0bbf5e70148f84
Your Name <you@example.com>
2023-12-09 11:26:24 -0800

    All html

--- forms.py
+++ forms.py
@@ -1,11 +1,12 @@
+
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, NumberRange, Email
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, IntegerField
-from wtforms.validators import InputRequired, Length
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(),Length(min=1, max=20)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=6, max=55)])
-    email = EmailField("Email", validators=[InputRequired(), Length(max=50)])
+    email = StringField("Email", validators=[InputRequired(), Email(), Length(max=50)])
     first_name = StringField("First Name", validators=[InputRequired(), Length(max=30)])
     last_name = StringField("Last Name", validators=[InputRequired(), Length(max=30)])
 
@@ -15,10 +16,8 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[InputRequired(), Length(min=6, max=55)])
 
 class FeedbackForm(FlaskForm):
-    id = IntegerField("user id", validators=[InputRequired()])
-    title = StringField("Title", validators=[InputRequired()])
-    content = StringField("Content", validators=[InputRequired()])
-    username = StringField("username", validators=[InputRequired()])
+    title = StringField("Title",validators=[InputRequired(), Length(max=100)],)
+    content = StringField("Content",validators=[InputRequired()],)
 
 class DeleteForm(FlaskForm):
     """Delete form -- this form is intentionally blank."""
